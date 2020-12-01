a8b817dfa760155f6a6e80c7d33d9d716b66e0cc
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-11-30 20:55:23 -0600

    changed designer from foreign key to string

--- src/components/game/GameDetails.js
+++ src/components/game/GameDetails.js
@@ -3,35 +3,34 @@ import { GameContext } from "./GameProvider.js"
 
 
 export const GameDetails = (props) => {
-    const { games, designers, getGameById, getDesignerById, getReviewsById, reviews } = useContext(GameContext)
+    const { games, getGameById, getReviewsById, reviews } = useContext(GameContext)
 
-    const [setGames, setDesigners, setReviews] = useState()
 
     useEffect(() => {
         const reviewId = parseInt()
         getReviewsById(reviewId)
-            .then(setReviews)
+
     
         const gameId = parseInt(props.match.params.gameId)
         getGameById(gameId)
-            .then(setGames)
+ 
 
-        const designerId = parseInt()
-        getDesignerById(designerId)
-            .then(setDesigners) //I don't think props.match.params.designerId is in the data
+        // const designerId = (games.designer_id.name)
+        // getDesignerById(designerId)
   }, [])
 
     return (
         <article>
           <section className="game">
               <div className="game__title">Title: {games.title}</div>
-              {/* <div className="game__designer">Designer: {games.designer_id}</div> */}
+              <div className="game__designer">Designer: {games.designer}</div>
               <div className="game__year">Year Released: {games.year_released}</div>
               <div className="game__players">{games.number_of_players} players needed</div>
               <div className="game__time">Estimated time to play: {games.est_time_to_play} hours</div>
               <div className="game__age">Age recommendation: minimum {games.age_recommendation} years old</div>
               <div className="game__categories">Categories: </div>
           </section>
+          {console.error(games.designer_id)}
         {
   
             reviews.map(review => {
@@ -45,6 +44,8 @@ export const GameDetails = (props) => {
                 props.history.push({ pathname: "/review" })
                 }}
                 >Review Game</button>
+
         </article>
+        
         )
     }
